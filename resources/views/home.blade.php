1965f73e64db806c55897a603cc740b10d42497a
Cris-NTL <natolicristian92@gmail.com>
2023-05-24 18:41:51 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row row-cols-5 g-3 pt-5">
+        @foreach ($movies as $movie)
+            <div class="col">
+                <div class="card border-0 mb-3">
+                    <div class="img">
+                        <img src="{{ $movie->image }}" alt="poster">
+                    </div>
+                    <div class="card-body p-0">
+                        <p>{{ $movie->title }}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
